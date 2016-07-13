0b93e05bcebef257e6a52f7f2c2b6c155f92bf53
grenyer <richard.grenyer@ouce.ox.ac.uk>
2016-07-13 12:00:18 +0100

    Revised data generator to add user-specified number of data and a cleaner scatter addition that doesn't use jitter().

--- DataGenerator.R
+++ DataGenerator.R
@@ -1,18 +1,12 @@
 #TEST CASES FOR GIT 
 
+ndata<-1000
 coef1<-runif(n = 1,min=-3,max=3)
 coef2<-runif(n = 1,min=-2,max=2)
-myj<-runif(n = 1,min=0,max=25)
+myj<-r(n = 1,min=0,max=25)
 
-b<-rbinom(1000, shape = 1)
+b<-runif(ndata)
 b2<-b^2
-c<-jitter(x=(coef1*b)+coef2*(b^2), amount=myj)
-plot(c~b)
+c<-(coef1*b)+coef2*(b2)+rnorm(ndata,mean = 0,sd = 0.25)
+#c<-jitter(x=(coef1*b)+coef2*(b^2), amount=myj)
 
-mylm<-lm(c~b+b2)
-print(summary(mylm))
-cat(coef1,coef2)
-
-predb<-seq(from=min(b),to=max(b),length.out = 20)
-mypred<-predict(object = mylm, newdata = list(b=predb,b2=predb^2))
-lines(predb, mypred, col="red", lwd=2)
--- ModelAndPlot.R
+++ ModelAndPlot.R
@@ -7,3 +7,4 @@ cat(coef1,coef2)
 predb<-seq(from=min(b),to=max(b),length.out = 20)
 mypred<-predict(object = mylm, newdata = list(b=predb,b2=predb^2))
 lines(predb, mypred, col="red", lwd=2)
+
